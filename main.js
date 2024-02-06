4173779c6bb504f342611fb097463aa166725f5a
eviecutrell <evelynn.cutrell@alaskaair.com>
2024-02-06 09:37:11 -0800

    media.js class created

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 // Import classes here to console.log and debug
+const Media = require("./classes/Media.js");
+
+//console.log(Media.totalMediaCount);
+
+//const mediaTest1 = new Media ("title1", 2000, "genre"); 
+//const mediaTest2 = new Media ("title2", 2000, "genre"); 
+
+//console.log(Media.totalMediaCount);
