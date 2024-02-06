617fbdbd8a1f6576233dfed8812d0a1ace19d1bc
Alex Evans <aevans@quiktrip.com>
2024-02-06 12:29:43 -0600

    Update Movie.js

--- classes/Movie.js
+++ classes/Movie.js
@@ -1,6 +1,32 @@
 // import the Media class:
-
+const Media = require("./Media.js");
 // create your Movie class:
 
+class Movie extends Media{
+    constructor(title, year, genre, director, duration, rating){
+        super(title, year, genre)
+        this.director = director;
+        this.duration = duration;
+        this.rating = rating;
+    }
+
+    summary(){
+        return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Rating: ${this.rating}`;
+    }
+
+    static longestMovie(arr){
+        //grab the first movie
+        let currentLongest = arr[0];
+        //loop through the movies and check duration vs current longest duration
+        arr.forEach(Movie => {
+            if(currentLongest.duration < Movie.duration){
+                currentLongest = Movie
+            }
+        });
+        //replace with duration if longer
+        //return the MOVIE with longest duration
+        return currentLongest
+    }
+}
 // don't change below
 module.exports = Movie;
