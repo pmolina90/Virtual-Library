334560ffd27006def511ee44b2e1f6bd61a50105
emartincek1 <evan.martincek@gmail.com>
2024-02-06 12:40:57 -0600

    add music class

--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,24 @@
 // import the Media class:
-
+const Media = require("./Media.js");
 // create your Music class:
-
+class Music extends Media {
+  constructor(title, genre, year, artist, length) {
+    super(title, genre, year);
+    this.artist = artist;
+    this.length = length;
+  }
+  summary() {
+    return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`;
+  }
+  static shortestAlbum(arr) {
+    let shortestSong = arr[0];
+    arr.forEach((song) => {
+      if (song.length < shortestSong.length) {
+        shortestSong = song;
+      }
+    });
+    return shortestSong;
+  }
+}
 // don't change below
 module.exports = Music;
--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 // Import classes here to console.log and debug
 const Media = require("./classes/Media.js");
-const Book = require("./classes/Book.js")
+const Book = require("./classes/Book.js");
+const Movie = require("./classes/Movie.js");
+const Music = require("./classes/Music.js");
 
 //console.log(Media.totalMediaCount);
 
-//const mediaTest1 = new Media ("title1", 2000, "genre"); 
-//const mediaTest2 = new Media ("title2", 2000, "genre"); 
+//const mediaTest1 = new Media ("title1", 2000, "genre");
+//const mediaTest2 = new Media ("title2", 2000, "genre");
 
 //console.log(Media.totalMediaCount);
